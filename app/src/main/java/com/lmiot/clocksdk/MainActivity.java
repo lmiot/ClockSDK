4a6f57360e868fcdfaf636d41e09b147e696679b
Administrator <alijiahua@126.com>
2018-03-26 18:58:01 +0800

    Initial commit

---
+++ app/src/main/java/com/lmiot/clocksdk/MainActivity.java
@@ -0,0 +1,19 @@
+package com.lmiot.clocksdk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.loonggg.lib.alarmmanager.clock.AlarmManagerUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AlarmManagerUtil. setAlarm(this, 1, 18, 56, 12, 0, "闹钟提醒", 2);
+        AlarmManagerUtil. setAlarm(this, 1, 18, 57, 12, 0, "闹钟提醒", 2);
+
+    }
+}
